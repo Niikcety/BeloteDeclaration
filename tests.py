a01c7003092a0a825b0843205fe1c84a2a080e34
Niikcety <nikolaineshev1999@gmail.com>
2020-03-30 18:59:18 +0300

    Fixed indents

--- tests.py
+++ tests.py
@@ -2,96 +2,96 @@ import unittest
 from utils import declarations_in, numbify, carre_in, seq_in, set_lad, validate_all
 
 class TestFunctionsInUtils(unittest.TestCase):
-	
-	def test_numbify(self):
-		hand = ['7C', 'JD', 'AS', 'QD', '8C', '9S', '10D', 'JS']
+    
+    def test_numbify(self):
+        hand = ['7C', 'JD', 'AS', 'QD', '8C', '9S', '10D', 'JS']
 
-		result = numbify(hand)
-		expected = [0, 1, 12, 13, 14, 29, 31, 34]
+        result = numbify(hand)
+        expected = [0, 1, 12, 13, 14, 29, 31, 34]
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_hand_with_carre_of_9s(self):
-		hand = ['7C', '9D', 'AS', 'QD', '9C', '9S', '10D', '9H']
-		cards = numbify(hand)
+    def test_hand_with_carre_of_9s(self):
+        hand = ['7C', '9D', 'AS', 'QD', '9C', '9S', '10D', '9H']
+        cards = numbify(hand)
 
-		result = carre_in(cards)
-		expected = [[2, 11, 20, 29]]
+        result = carre_in(cards)
+        expected = [[2, 11, 20, 29]]
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_hand_with_two_carres(self):
-		hand = ['AC', 'AD', 'AH', 'AS', '9C', '9S', '9D', '9H']
-		cards = numbify(hand)
+    def test_hand_with_two_carres(self):
+        hand = ['AC', 'AD', 'AH', 'AS', '9C', '9S', '9D', '9H']
+        cards = numbify(hand)
 
-		result = carre_in(cards)
-		expected = [[2, 11, 20, 29], [7, 16, 25, 34]]
+        result = carre_in(cards)
+        expected = [[2, 11, 20, 29], [7, 16, 25, 34]]
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_hand_with_carre_of_7s(self):
-		hand = ['7C', '7D', 'AS', 'QD', '9C', '7S', '10D', '7H']
-		cards = numbify(hand)
+    def test_hand_with_carre_of_7s(self):
+        hand = ['7C', '7D', 'AS', 'QD', '9C', '7S', '10D', '7H']
+        cards = numbify(hand)
 
-		result = carre_in(cards)
-		expected = []
+        result = carre_in(cards)
+        expected = []
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_hand_with_belote_and_two_tierces(self):
-		hand = ['KH', 'QH', '8C', '7C', '9C', '9D', '10D', 'JD']
-		cards = numbify(hand)
+    def test_hand_with_belote_and_two_tierces(self):
+        hand = ['KH', 'QH', '8C', '7C', '9C', '9D', '10D', 'JD']
+        cards = numbify(hand)
 
-		result = seq_in(cards)
-		expected = [[0, 1, 2], [11, 12, 13], [23, 24]]
+        result = seq_in(cards)
+        expected = [[0, 1, 2], [11, 12, 13], [23, 24]]
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_hand_with_carre_and_tierce_thats_part_of_the_carre(self):
-		hand = ['AC', '9H', '8C', '9S', '9C', '9D', '10D', 'JD']
-		cards = numbify(hand)
-		control = carre_in(cards)
+    def test_hand_with_carre_and_tierce_thats_part_of_the_carre(self):
+        hand = ['AC', '9H', '8C', '9S', '9C', '9D', '10D', 'JD']
+        cards = numbify(hand)
+        control = carre_in(cards)
 
-		result = seq_in(cards, control)
-		expected = []
+        result = seq_in(cards, control)
+        expected = []
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_hand_with_nothing_in_it(self):
-		hand = ['AC', '9H', '8C', '9S', '7C', '9D', '10D', 'QD']
+    def test_hand_with_nothing_in_it(self):
+        hand = ['AC', '9H', '8C', '9S', '7C', '9D', '10D', 'QD']
 
-		result = declarations_in(hand)
-		expected = [[],[]]
+        result = declarations_in(hand)
+        expected = [[],[]]
 
 
-	def test_setting_of_lad(self):
-		an1 = [[5, 6], [5, 6, 7]]
-		an2 = [[28, 29, 30, 31]]
-		opp_ann = an1+an2
+    def test_setting_of_lad(self):
+        an1 = [[5, 6], [5, 6, 7]]
+        an2 = [[28, 29, 30, 31]]
+        opp_ann = an1+an2
 
-		result = set_lad(opp_ann)
-		expected = 4.1
+        result = set_lad(opp_ann)
+        expected = 4.1
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_setting_of_lad_with_empty_opp_announcements(self):
-		an1 = [[], []]
-		an2 = [[]]
-		opp_ann = an1+an2
+    def test_setting_of_lad_with_empty_opp_announcements(self):
+        an1 = [[], []]
+        an2 = [[]]
+        opp_ann = an1+an2
 
-		result = set_lad(opp_ann)
-		expected = 0
+        result = set_lad(opp_ann)
+        expected = 0
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
-	def test_invalid_tierce_and_valid_quinte(self):
-		ann = [[0, 1, 2],[9, 8, 10, 11, 12]]
-		lad = 4
-		
-		result = validate_all(ann, 4)
-		expected = [[9, 8, 10, 11, 12]]
+    def test_invalid_tierce_and_valid_quinte(self):
+        ann = [[0, 1, 2],[9, 8, 10, 11, 12]]
+        lad = 4
+        
+        result = validate_all(ann, 4)
+        expected = [[9, 8, 10, 11, 12]]
 
-		self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- utils.py
+++ utils.py
@@ -11,93 +11,93 @@ of different suites in the dictionary below.'''
 seq_dict = {k: v for k, v in zip(deck, num)}
 
 seq_hierarchy = {
-	(0, 1, 2) : ['tierce', 3.0],
-	(1, 2, 3) : ['tierce', 3.1],
+    (0, 1, 2) : ['tierce', 3.0],
+    (1, 2, 3) : ['tierce', 3.1],
     (2, 3, 4) : ['tierce', 3.2],
     (3, 4, 5) : ['tierce', 3.3],
     (4, 5, 6) : ['tierce', 3.4],
     (5, 6, 7) : ['tierce', 3.5],
-	(0, 1, 2, 3) : ['quarte', 4.0],
-	(1, 2, 3, 4) : ['quarte', 4.1],
-	(2, 3, 4, 5) : ['quarte', 4.2],
-	(3, 4, 5, 6) : ['quarte', 4.3],
-	(4, 5, 6, 7) : ['quarte', 4.4],
-	(0, 1, 2, 3, 4) : ['quinte', 5.0],
-	(1, 2, 3, 4, 5) : ['quinte', 5.1],
-	(2, 3, 4, 5, 6) : ['quinte', 5.2],
-	(3, 4, 5, 6, 7) : ['quinte', 5.3],
-	(9, 10, 11) : ['tierce', 3.0],
-	(10, 11, 12) : ['tierce', 3.1], 
-	(11, 12, 13) : ['tierce', 3.2],
-	(12, 13, 14) : ['tierce', 3.3],
-	(13, 14, 15) : ['tierce', 3.4],
-	(14, 15, 16) : ['tierce', 3.5],
-	(9, 10, 11, 12) : ['quarte', 4.0],
-	(10, 11, 12, 13) : ['quarte', 4.1], 
-	(11, 12, 13, 14) : ['quarte', 4.2],
-	(12, 13, 14, 15) : ['quarte', 4.3],
-	(13, 14, 15, 16) : ['quarte', 4.4],
-	(9, 10, 11, 12, 13) : ['quinte', 5.0],
-	(10, 11, 12, 13, 14) : ['quinte', 5.1], 
-	(11, 12, 13, 14, 15) : ['quinte', 5.2],
-	(12, 13, 14, 15, 16) : ['quinte', 5.3],
-	(18, 19, 20) : ['tierce', 3.0],
-	(19, 20, 21) : ['tierce', 3.1],
-	(20, 21, 22) : ['tierce', 3.2],
-	(21, 22, 23) : ['tierce', 3.3],
-	(22, 23, 24) : ['tierce', 3.4],
-	(23, 24, 25) : ['tierce', 3.5],
-	(18, 19, 20, 21) : ['quarte', 4.0],
-	(19, 20, 21, 22) : ['quarte', 4.1],
-	(20, 21, 22, 23) : ['quarte', 4.2],
-	(21, 22, 23, 24) : ['quarte', 4.3],
-	(22, 23, 24, 25) : ['quarte', 4.4],
-	(18, 19, 20, 21, 22) : ['quinte', 5.0],
-	(19, 20, 21, 22, 23) : ['quinte', 5.1],
-	(20, 21, 22, 23, 24) : ['quinte', 5.2],
-	(21, 22, 23, 24, 25) : ['quinte', 5.3],
-	(27, 28, 29) : ['tierce', 3.0],
-	(28, 29, 30) : ['tierce', 3.1], 
-	(29, 30, 31) : ['tierce', 3.2],
-	(30, 31, 32) : ['tierce', 3.3],
-	(31, 32, 33) : ['tierce', 3.4],
-	(32, 33, 34) : ['tierce', 3.5],
-	(27, 28, 29, 30) : ['quarte', 4.0],
-	(28, 29, 30, 31) : ['quarte', 4.1],
-	(29, 30, 31, 32) : ['quarte', 4.2],
-	(30, 31, 32, 33) : ['quarte', 4.3],
-	(31, 32, 33, 34) : ['quarte', 4.4],
-	(27, 28, 29, 30, 31) : ['quinte', 5.0],
-	(28, 29, 30, 31, 32) : ['quinte', 5.1],
-	(29, 30, 31, 32, 33) : ['quinte', 5.2],
-	(30, 31, 32, 33, 34) : ['quinte', 5.3]
-	}
+    (0, 1, 2, 3) : ['quarte', 4.0],
+    (1, 2, 3, 4) : ['quarte', 4.1],
+    (2, 3, 4, 5) : ['quarte', 4.2],
+    (3, 4, 5, 6) : ['quarte', 4.3],
+    (4, 5, 6, 7) : ['quarte', 4.4],
+    (0, 1, 2, 3, 4) : ['quinte', 5.0],
+    (1, 2, 3, 4, 5) : ['quinte', 5.1],
+    (2, 3, 4, 5, 6) : ['quinte', 5.2],
+    (3, 4, 5, 6, 7) : ['quinte', 5.3],
+    (9, 10, 11) : ['tierce', 3.0],
+    (10, 11, 12) : ['tierce', 3.1], 
+    (11, 12, 13) : ['tierce', 3.2],
+    (12, 13, 14) : ['tierce', 3.3],
+    (13, 14, 15) : ['tierce', 3.4],
+    (14, 15, 16) : ['tierce', 3.5],
+    (9, 10, 11, 12) : ['quarte', 4.0],
+    (10, 11, 12, 13) : ['quarte', 4.1], 
+    (11, 12, 13, 14) : ['quarte', 4.2],
+    (12, 13, 14, 15) : ['quarte', 4.3],
+    (13, 14, 15, 16) : ['quarte', 4.4],
+    (9, 10, 11, 12, 13) : ['quinte', 5.0],
+    (10, 11, 12, 13, 14) : ['quinte', 5.1], 
+    (11, 12, 13, 14, 15) : ['quinte', 5.2],
+    (12, 13, 14, 15, 16) : ['quinte', 5.3],
+    (18, 19, 20) : ['tierce', 3.0],
+    (19, 20, 21) : ['tierce', 3.1],
+    (20, 21, 22) : ['tierce', 3.2],
+    (21, 22, 23) : ['tierce', 3.3],
+    (22, 23, 24) : ['tierce', 3.4],
+    (23, 24, 25) : ['tierce', 3.5],
+    (18, 19, 20, 21) : ['quarte', 4.0],
+    (19, 20, 21, 22) : ['quarte', 4.1],
+    (20, 21, 22, 23) : ['quarte', 4.2],
+    (21, 22, 23, 24) : ['quarte', 4.3],
+    (22, 23, 24, 25) : ['quarte', 4.4],
+    (18, 19, 20, 21, 22) : ['quinte', 5.0],
+    (19, 20, 21, 22, 23) : ['quinte', 5.1],
+    (20, 21, 22, 23, 24) : ['quinte', 5.2],
+    (21, 22, 23, 24, 25) : ['quinte', 5.3],
+    (27, 28, 29) : ['tierce', 3.0],
+    (28, 29, 30) : ['tierce', 3.1], 
+    (29, 30, 31) : ['tierce', 3.2],
+    (30, 31, 32) : ['tierce', 3.3],
+    (31, 32, 33) : ['tierce', 3.4],
+    (32, 33, 34) : ['tierce', 3.5],
+    (27, 28, 29, 30) : ['quarte', 4.0],
+    (28, 29, 30, 31) : ['quarte', 4.1],
+    (29, 30, 31, 32) : ['quarte', 4.2],
+    (30, 31, 32, 33) : ['quarte', 4.3],
+    (31, 32, 33, 34) : ['quarte', 4.4],
+    (27, 28, 29, 30, 31) : ['quinte', 5.0],
+    (28, 29, 30, 31, 32) : ['quinte', 5.1],
+    (29, 30, 31, 32, 33) : ['quinte', 5.2],
+    (30, 31, 32, 33, 34) : ['quinte', 5.3]
+    }
 
 def validate_all(all_ann, lad):
-	control = []
+    control = []
 
-	for ann in all_ann:
-		
-		if tuple(ann) not in seq_hierarchy:
-			control.append(ann)
-		elif seq_hierarchy[tuple(ann)][1] >= lad:
-			control.append(ann)
+    for ann in all_ann:
+        
+        if tuple(ann) not in seq_hierarchy:
+            control.append(ann)
+        elif seq_hierarchy[tuple(ann)][1] >= lad:
+            control.append(ann)
 
-	return control
+    return control
 
 
 def set_lad(opposing_ann):
-	control = []
+    control = []
 
-	for ann in opposing_ann:
-		
-		if tuple(ann) in seq_hierarchy:
-			control.append(seq_hierarchy[tuple(ann)][1])
+    for ann in opposing_ann:
+        
+        if tuple(ann) in seq_hierarchy:
+            control.append(seq_hierarchy[tuple(ann)][1])
 
-	try:
-		return max(control)
-	except:
-		return 0
+    try:
+        return max(control)
+    except:
+        return 0
 
 
 def resolve(sequences, control):
@@ -106,13 +106,13 @@ def resolve(sequences, control):
     for seq in sequences:
 
         if len(seq) == 1:
-        	pass
+            pass
 
         elif len(seq) == 2 and seq == [5, 6] or seq == [14, 15] or seq == [23, 24] or seq == [32, 33]:
-        	resolved_s.append(seq)
+            resolved_s.append(seq)
         
         elif len(seq) > 2 and not any(c in seq for con in control for c in con):
-        	resolved_s.append(seq)
+            resolved_s.append(seq)
 
     return resolved_s
 
@@ -135,51 +135,50 @@ def seq_in(cards, control=[]):
     return resolve(sequences, control)
 
 def carre_in(cards):
-	result = []
+    result = []
 
-	for i in cards:
+    for i in cards:
 
-		if i == 0 or i == 1:
-			pass
+        if i == 0 or i == 1:
+            pass
 
-		elif i == 2:
-			if i+9 and i+18 and i+27 in cards:
-				result.append([2, 11, 20, 29])
+        elif i == 2:
+            if i+9 and i+18 and i+27 in cards:
+                result.append([2, 11, 20, 29])
 
-		elif i == 3:
-			if i+9 and i+18 and i+27 in cards:
-				result.append([3, 12, 21, 30])
+        elif i == 3:
+            if i+9 and i+18 and i+27 in cards:
+                result.append([3, 12, 21, 30])
 
-		elif i == 4:
-			if i+9 and i+18 and i+27 in cards:
-				result.append([4, 13, 22, 31])
+        elif i == 4:
+            if i+9 and i+18 and i+27 in cards:
+                result.append([4, 13, 22, 31])
 
-		elif i == 5:
-			if i+9 and i+18 and i+27 in cards:
-				result.append([5, 14 ,23, 32])
+        elif i == 5:
+            if i+9 and i+18 and i+27 in cards:
+                result.append([5, 14 ,23, 32])
 
-		elif i == 6:
-			if i+9 and i+18 and i+27 in cards:
-				result.append([6, 15, 24, 33])
+        elif i == 6:
+            if i+9 and i+18 and i+27 in cards:
+                result.append([6, 15, 24, 33])
 
-		elif i == 7:
-			if i+9 and i+18 and i+27 in cards:
-				result.append([7, 16, 25, 34])
-	return result
+        elif i == 7:
+            if i+9 and i+18 and i+27 in cards:
+                result.append([7, 16, 25, 34])
+    return result
 
 def numbify(hand):
-	cards = []
+    cards = []
 
-	for card in hand:
-		cards.append(seq_dict[card])
+    for card in hand:
+        cards.append(seq_dict[card])
 
-	return sorted(cards)
+    return sorted(cards)
 
 def declarations_in(hand):
-	cards = numbify(hand)
-	carre = carre_in(cards)
-	seqs = seq_in(cards, carre)
+    cards = numbify(hand)
+    carre = carre_in(cards)
+    seqs = seq_in(cards, carre)
 
-	return [seqs, carre]
+    return [seqs, carre]
 
-	
